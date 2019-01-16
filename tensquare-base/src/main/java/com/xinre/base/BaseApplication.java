54b7dbc1231fa94be7854326641321fa6f19ce1d
mfl1249 <mfanlong@sina.com>
2019-01-16 17:09:48 +0800

    提交common模块与base模块

---
+++ tensquare-base/src/main/java/com/xinre/base/BaseApplication.java
@@ -0,0 +1,20 @@
+package com.xinre.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author xinre
+ * @date 2019/1/16 16:54
+ */
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class);
+    }
+
+
+}
