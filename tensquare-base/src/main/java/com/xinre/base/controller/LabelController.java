44b7061519c1de1e79360e1d4a4e43f706500756
mfl1249 <mfanlong@sina.com>
2019-01-18 10:48:38 +0800

    提交rest简单接口demo

--- tensquare-base/src/main/java/com/xinre/base/controller/LabelController.java
+++ tensquare-base/src/main/java/com/xinre/base/controller/LabelController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @author xinre
  * @date 2019/1/17 17:21
@@ -28,28 +30,45 @@ public class LabelController {
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll() {
-        return new Result(StatusCode.OK, true, "方法 findAll 成功！");
+        List<Label> labels = labelService.findAll();
+        return new Result(StatusCode.OK, true, "方法 findAll 成功！", labels);
     }
 
     @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Result findById(@PathVariable(name = "id") String labelId) {
-        return new Result(StatusCode.OK, true, "方法 findById 成功！", labelId);
+        Label label = labelService.findById(labelId);
+        return new Result(StatusCode.OK, true, "方法 findById 成功！", label);
     }
 
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST)
     public Result save(@RequestBody Label label) {
-        //Label label = new Label();
-        //label.setId(String.valueOf(idWorker.nextId()));
-        label.setCount(5L);
-        label.setFans(95L);
-        label.setName("java");
-        label.setState("1");
-        label.setRecommend("1");
+
+        label.setId(String.valueOf(idWorker.nextId()));
+//        label.setCount(5L);
+//        label.setFans(95L);
+//        label.setName("java");
+//        label.setState("1");
+//        label.setRecommend("1");
+
         return new Result(StatusCode.OK, true, "方法 save 成功！", label);
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Result updateById(@PathVariable(name = "id") String labelId, @RequestBody Label label) {
+        label.setId(labelId);
+        labelService.update(label);
+        return new Result(StatusCode.OK, true, "方法 updateById 成功！");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable(name = "id") String labelId) {
+        labelService.deleteById(labelId);
+        return new Result(StatusCode.OK, true, "方法 deleteById 成功！");
+    }
 
 
 }
